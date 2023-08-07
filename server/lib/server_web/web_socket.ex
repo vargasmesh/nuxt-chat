4ca607dba60541fd17afab69a8d946df2d77acb6
Lucas Vargas Noronha <lvargasnoronha@gmail.com>
2023-08-06 21:37:18 -0300

    feat: style messages

--- server/lib/server_web/web_socket.ex
+++ server/lib/server_web/web_socket.ex
@@ -1,19 +1,20 @@
 defmodule ServerWeb.WebSocket do
   alias Phoenix.PubSub
 
-  @type state :: %{user: String.t(), id: String.t()}
+  @type state :: %{username: String.t(), user_id: String.t()}
 
   @spec init(nil | [user: String.t()]) :: {:ok, state()}
   def init(options) do
     PubSub.subscribe(Server.PubSub, "all")
-    {:ok, %{user: options[:user], id: UUID.uuid4()}}
+    {:ok, %{username: options[:user], user_id: UUID.uuid4()}}
   end
 
-  def handle_in({message, [opcode: :text]}, %{user: user, id: id} = state) do
+  def handle_in({message, [opcode: :text]}, %{username: username, user_id: user_id} = state) do
     payload =
       Jason.decode!(message)
-      |> Map.put("user", user)
-      |> Map.put("id", id)
+      |> Map.put("username", username)
+      |> Map.put("user_id", user_id)
+      |> Map.put("id", UUID.uuid4())
 
     PubSub.broadcast(
       Server.PubSub,
@@ -24,11 +25,8 @@ defmodule ServerWeb.WebSocket do
     {:ok, state}
   end
 
-  def handle_info({:broadcast, message}, %{id: id} = state) do
-    IO.inspect(message)
-    IO.puts(id)
-
-    if message["id"] == id do
+  def handle_info({:broadcast, message}, %{user_id: user_id} = state) do
+    if message["user_id"] == user_id do
       {:ok, state}
     else
       {:push, {:text, Jason.encode!(message)}, state}
--- web/pages/chat.vue
+++ web/pages/chat.vue
@@ -6,8 +6,19 @@
                 <div class="text-sm">logged as {{ user }}</div>
             </div>
             <div class="flex flex-col py-4">
-                <div v-for="message in messages" :key="message" hover="bg-#282b30" class="py-2">
-                    {{ message }}
+                <div v-for="message in messages" :key="message.id" hover="bg-#282b30"
+                    class="py-2 flex gap-4 items-center px-1">
+                    <div class="p-3 bg-#7289da rounded-full text-center">
+                        {{ getInitials(message.username) }}
+                    </div>
+                    <div>
+                        <div class="font-semibold">
+                            {{ message.username }}
+                        </div>
+                        <div>
+                            {{ message.message }}
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
@@ -18,14 +29,32 @@
 const user = useUser()
 const router = useRouter();
 const ws = ref<WebSocket>()
-const messages = ref<string[]>([])
+
+type WebSocketMessage = {
+    id: string
+    message: string
+    user_id: string
+    username: string
+}
+
+const messages = ref<WebSocketMessage[]>([])
+
+function getInitials(name: string) {
+    let initials = name.split(" ").map((n) => n[0]).join("").substring(0, 2)
+    if (initials.length < 2) {
+        initials = name.substring(0, 2)
+    }
+    return initials.toUpperCase()
+}
+
 
 if (!user.value) {
     router.push("/")
 } else {
     ws.value = new WebSocket("ws://192.168.31.177:8000/ws")
-    ws.value.onmessage = (e) => {
-        messages.value.push(e.data)
+    ws.value.onmessage = (e: MessageEvent) => {
+        const message = JSON.parse(e.data) as WebSocketMessage
+        messages.value.push(message)
     }
 }
 
